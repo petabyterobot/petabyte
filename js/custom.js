88c32853c059a8131fbaff7e00d982f38dc8219d
ali yaqoubi <123yqb@gmail.com>
2020-08-26 03:31:13 +0430

    init

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+var _0x2d40 = [
+    "scrollTop",
+    ".navbar-mobile\x20.nav-item",
+    "removeClass",
+    "scroll",
+    "replace",
+    "header-scrolled",
+    "delay",
+    "",
+    "width",
+    "owlCarousel",
+    ".owl-testimonials-slider",
+    ".navbar-toggler",
+    "hostname",
+    "fadeOut",
+    "لطفا\x20قالب\x20را\x20خریداری\x20کنید",
+    "trigger",
+    "location",
+    "click",
+  ];
+  (function (_0x40a36e, _0x2d40ed) {
+    var _0x20a71f = function (_0x4dabb6) {
+      while (--_0x4dabb6) {
+        _0x40a36e["push"](_0x40a36e["shift"]());
+      }
+    };
+    _0x20a71f(++_0x2d40ed);
+  })(_0x2d40, 0x173);
+  var _0x20a7 = function (_0x40a36e, _0x2d40ed) {
+    _0x40a36e = _0x40a36e - 0x0;
+    var _0x20a71f = _0x2d40[_0x40a36e];
+    return _0x20a71f;
+  };
+  $(function () {
+    "use strict";
+    jQuery("#preloader")[_0x20a7("0xd")](0x1f4)[_0x20a7("0x2")](0x1f4);
+    $(window)[_0x20a7("0xa")](function () {
+      $(this)[_0x20a7("0x7")]() > 0x14
+        ? $("#navbar")["addClass"](_0x20a7("0xc"))
+        : $("#navbar")[_0x20a7("0x9")](_0x20a7("0xc"));
+    });
+    $(_0x20a7("0x11"))[_0x20a7("0x10")]({
+      items: 0x1,
+      loop: !![],
+      nav: !![],
+      autoplay: !![],
+      autoplayTimeout: 0x1388,
+      autoplayHoverPause: ![],
+      responsiveClass: !![],
+      responsive: {
+        0: { items: 0x1 },
+        600: { items: 0x1 },
+        1000: { items: 0x1 },
+      },
+    });
+    var _0x161201 = $(window)[_0x20a7("0xf")]();
+    _0x161201 <= 0x301 &&
+      $(_0x20a7("0x8"))["on"](_0x20a7("0x6"), function () {
+        $(_0x20a7("0x0"))[_0x20a7("0x4")](_0x20a7("0x6"));
+      });
+  });
+  window[_0x20a7("0x5")][_0x20a7("0x1")] != _0x20a7("0xe") &&
+    (alert(_0x20a7("0x3")),
+    window["location"][_0x20a7("0xb")](
+      "https://www.rtl-theme.com/author/garousiamir/"
+    ));
+  
